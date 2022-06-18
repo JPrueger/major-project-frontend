a63ec205a190acb5056c6b31064d65d6c0affe8d
Jeannine Prueger <jeannine.prueger@towa.at>
2022-06-18 19:51:20 +0200

    feat: add GraphQl component

---
+++ src/components/GraphQL.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="max-w-lg mx-auto relative">
+    {{ todos.data.allTodos.nodes }}
+  </div>
+</template>
+<script>
+import { GET_ALL_TODOS_QUERY } from './graphql/queries/allTodos'
+
+export default {
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  async mounted () {
+    this.loading = true
+    this.todos = await this.$apollo.query({ query: GET_ALL_TODOS_QUERY })
+    this.loading = false
+  },
+};
+</script>
