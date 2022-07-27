e8e20904629d80a636799d806c98b59addf10c2c
Jeannine Prueger <jeannine.prueger@towa.at>
2022-07-27 21:42:41 +0200

    feat: add pdf download to graphql

--- src/components/graphql-component.vue
+++ src/components/graphql-component.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="max-w-lg mx-auto relative">
-    {{ todos.data.allTodos.nodes }}
+    <pre>
+      <!-- {{ todos.data.allTodos.nodes }} -->
+      <!-- document: -->
+      {{ document }}
+      <!-- {{ todos }} -->
+    </pre>
   </div>
 </template>
 <script>
@@ -10,12 +15,20 @@ export default {
   data() {
     return {
       todos: [],
+      document: '',
     };
   },
   async mounted () {
     this.loading = true
     this.todos = await this.$apollo.query({ query: GET_ALL_TODOS_QUERY })
     this.loading = false
+    console.log(this.todos.data.allTodos.nodes[0].document);
+    const linkSource = `data:application/pdf;base64,${this.todos.data.allTodos.nodes[0].document}`;
+    const downloadLink = document.createElement("a");
+    const fileName = "abc.pdf";
+    downloadLink.href = linkSource;
+    downloadLink.download = fileName;
+    downloadLink.click();
   },
 };
 </script>
